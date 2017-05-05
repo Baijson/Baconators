7eb1f0673f9724b0a00708e6149ac9cff401492e
Baijson <dev@baijbot.tv>
2017-05-05 16:00:57 +0200

    Skeleton.

--- src/main/java/mod/baijson/baconators/Baconators.java
+++ src/main/java/mod/baijson/baconators/Baconators.java
@@ -9,16 +9,21 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 /**
  * File created by Baijson.
  */
-@Mod(modid = Baconators.MODID, name = Baconators.MODNM, version = Baconators.VERSION)
+@Mod(modid = Baconators.MODID, name = Baconators.MODNM, version = Baconators.VERSION, dependencies = Baconators.DEPENDENCIES)
 public class Baconators {
 
     public static final String MODID = "baconators";
     public static final String MODNM = "Baconators";
 
-    public static final String VERSION = "3.1.0";
+    public static final String VERSION = "3.1.1";
 
     public static final String PROXY_COMMON = "mod.baijson." + MODID + ".common.CommonProxy";
     public static final String PROXY_CLIENT = "mod.baijson." + MODID + ".client.ClientProxy";
+
+    /**
+     * Dependencies.
+     */
+    public static final String DEPENDENCIES = "required-after:skeleton@[1.1.0,);";
 
     /**
      * Instance
